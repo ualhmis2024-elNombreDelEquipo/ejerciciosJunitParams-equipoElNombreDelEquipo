f3319464b84530bdc10b1b2675c4fc49bafa900f
María Martínez <mmc303@inlumine.ual.es>
2024-04-09 13:51:48 +0200

    creacion de ejercicios

---
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio4Test.java
@@ -0,0 +1,10 @@
+package ual.hmis.sesion05.ejercicio4;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ual.hmis.sesion05.ejercicio4.*;
+
+ class Ejercicio4Test {
+
+}
---
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio5/Ejercicio5Test.java
@@ -0,0 +1,11 @@
+package ual.hmis.sesion05.ejercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ual.hmis.sesion05.ejercicio5.*;
+
+ class Ejercicio5Test {
+
+}
+
