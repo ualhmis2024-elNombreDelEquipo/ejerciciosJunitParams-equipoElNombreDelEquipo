c21edf3849e448d65af2eb2f0ee22cdde86e1bdb
Jose Carlos Ruiz Sanchez <jrs407@inlumine.ual.es>
2024-04-13 17:21:50 +0200

    Añadido ejercicio 4 junto al test

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio4/Ejercicio4.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio4/Ejercicio4.java
@@ -1,5 +1,48 @@
 package ual.hmis.sesion05.ejercicio4;
+import java.util.*;
 
-public class Ejercicio4 {
+public class Ejercicio4<T> {
 
+	
+	public List<T> MezclaLineal(List<T> lista1, List<T> lista2){
+		
+		if(lista1 == null && lista2 == null) {
+			return null;
+		}
+		
+		if(lista1 == null) {
+			lista2.sort(null);
+			return lista2;
+		}
+		
+		if(lista2 == null) {
+			lista1.sort(null);
+			return lista1;
+		}
+		
+		if(lista1.isEmpty() && lista2.isEmpty()) {
+			return lista1;
+		}
+		
+		if(lista2.isEmpty()) {
+			lista1.sort(null);
+			return lista1;
+		}
+		
+		if(lista1.isEmpty()) {
+			lista2.sort(null);
+			return lista2;
+		}
+		
+		for (T elemento : lista2) {
+			
+			if(!lista1.contains(elemento)) {
+				lista1.add(elemento);
+			}
+			
+		}
+		
+		lista1.sort(null);	
+		return lista1;
+	}
 }
