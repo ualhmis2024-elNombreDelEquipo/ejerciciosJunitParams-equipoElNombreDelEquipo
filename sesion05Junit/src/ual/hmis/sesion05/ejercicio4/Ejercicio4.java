211865b9225ff54469a13e8ebf3a7a6ad64f1bba
Jose Carlos Ruiz Sanchez <jrs407@inlumine.ual.es>
2024-04-16 13:51:16 +0200

    Arreglado ejercicio 4

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio4/Ejercicio4.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio4/Ejercicio4.java
@@ -6,20 +6,6 @@ public class Ejercicio4<T> {
 	
 	public List<T> MezclaLineal(List<T> lista1, List<T> lista2){
 		
-		if(lista1 == null && lista2 == null) {
-			return null;
-		}
-		
-		if(lista1 == null) {
-			lista2.sort(null);
-			return lista2;
-		}
-		
-		if(lista2 == null) {
-			lista1.sort(null);
-			return lista1;
-		}
-		
 		if(lista1.isEmpty() && lista2.isEmpty()) {
 			return lista1;
 		}
--- sesion05Junit/test/ual/hmis/sesion05/ejercicio5/Ejercicio5Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio5/Ejercicio5Test.java
@@ -1,11 +1,26 @@
 package ual.hmis.sesion05.ejercicio5;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import ual.hmis.sesion05.ejercicio5.*;
 
- class Ejercicio5Test {
+ public class Ejercicio5Test {
 
+	 private static String directorioEntrada = System.getProperty("user.dir") + File.separator + "src"
+				+ File.separator + "ual" +  File.separator + "hmis" + File.separator + "sesion05" +File.separator + "ejercicio5" +File.separator;
+	 
+	 public static void main(String[] args) {
+		 
+		 
+		 Ejercicio5 ejPrueba = new Ejercicio5();
+		 
+		 ArrayList<String> prueba = ejPrueba.ordenAlfabetico(directorioEntrada + "HMIS.txt");
+		 
+		 System.out.println(prueba.toString());
+	 }
 }
 
