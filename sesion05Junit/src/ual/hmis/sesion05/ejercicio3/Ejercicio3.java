99ac76391935cbb2c914d039254cbd72145e13d7
María Martínez <mmc303@inlumine.ual.es>
2024-04-16 14:27:52 +0200

    ejercicio3

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio3/Ejercicio3.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio3/Ejercicio3.java
@@ -1,5 +1,25 @@
 package ual.hmis.sesion05.ejercicio3;
 
 public class Ejercicio3 {
+	static String resultado = null;
 
+	public String enmascararPassword(String password) {
+		int length = password.length();
+		
+		if (length < 5) {
+			return "password demasiado corto";
+		} else if (length <= 8)  {
+			return "********";
+		}
+		if (length >= 12 && length <= 40) {
+			return "************";
+		}
+		if (length > 40) {
+			return "password demasiado largo";
+		}
+		for (int i = 0; i < length; i++) {
+			resultado += "*";
+		}
+		return resultado;
+	}
 }
